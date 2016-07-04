3a800a90faa36b02719c6b8b6f1aa65e4b7bbd8e
std4453 <std4453@foxmail.com>
2016-07-04 15:16:01 +0800

    Delete IMapper.java
    It is useless

--- src/terraingen/backend/nodegraph/IMapper.java
+++
@@ -1,8 +0,0 @@
-package terraingen.backend.nodegraph;
-
-/**
- * Maps a value of class V to a key of class K for route choosing
- */
-public interface IMapper<K, V> extends IComponent {
-	K map(V value);
-}
