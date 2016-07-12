27a1839ad08a8bc6c1a8733a2c22f844ae157594
std4453 <std4453@foxmail.com>
2016-07-12 23:58:44 +0800

    Add profiling for FortuneTest
    Well, actually, profiling shows that my codes runs plainly 2 times faster than the one of ajwerner. LOL

--- src/terraingen/test/FortuneTest.java
+++ src/terraingen/test/FortuneTest.java
@@ -1,13 +1,11 @@
 package terraingen.test;
 
-import org.ajwerner.voronoi.VoronoiAdapter;
 import terraingen.backend.commons.Boundaries;
 import terraingen.backend.commons.PointBox;
 import terraingen.backend.commons.random.PointsWhiteNoise;
 import terraingen.backend.commons.voronoi.Fortune;
 import terraingen.backend.commons.voronoi.VoronoiBox;
 import terraingen.backend.commons.voronoi.VoronoiRenderer;
-import terraingen.backend.nodegraph.Edge;
 import terraingen.backend.nodegraph.Executor;
 import terraingen.backend.nodegraph.ProcessorNode;
 import terraingen.backend.nodegraph.Statement;
@@ -20,26 +18,38 @@ import java.awt.image.BufferedImage;
  */
 public class FortuneTest {
 	public static void main(String[] args) {
-		final int points = 100;
-		final long seed = 10;
-//		final Boundaries boundaries = new Boundaries(0, 100, 0, 100);
-		final Boundaries boundaries = new Boundaries(-100, 100, -100, 100);
+		final int points = 50;
+		final long seed = 100;
+		final Boundaries boundaries = new Boundaries(0, 10, 0, 10);
 
 		ProcessorNode<Long, PointBox> randomPoints = new ProcessorNode<>(
 				new PointsWhiteNoise(boundaries, points));
-		ProcessorNode<PointBox, VoronoiBox> voronoi = null;
-		if (true)
-			voronoi = new ProcessorNode<>(new Fortune());
-		else
-			voronoi = new ProcessorNode<>(new VoronoiAdapter());
+		ProcessorNode<PointBox, VoronoiBox> voronoi = new ProcessorNode<>(new Fortune());
+//		ProcessorNode<PointBox, VoronoiBox> voronoi = new ProcessorNode<>(new
+//				VoronoiAdapter());
 		ProcessorNode<VoronoiBox, BufferedImage> renderer = new ProcessorNode<>(
 				new VoronoiRenderer());
-		new Edge<>(randomPoints.getOutput(), voronoi.getInput());
-		new Edge<>(voronoi.getOutput(), renderer.getInput());
-		Statement<Long, BufferedImage> pipeline = new Statement<>(randomPoints.getInput(),
-				renderer.getOutput());
 
-		BufferedImage image = Executor.execute(pipeline, seed);
+		// profiler components
+		Statement<Long, PointBox> pointsGenerator = new Statement<>(randomPoints);
+		PointBox pointBox = Executor.execute(pointsGenerator, seed);
+		Statement<PointBox, VoronoiBox> voronoiGenerator = new Statement<>(voronoi);
+
+		// profiler
+		long startTime = System.currentTimeMillis();
+		final int repeats = 1000;
+		for (int i = 0; i < repeats; ++i)
+			Executor.execute(voronoiGenerator, pointBox);
+		long endTime = System.currentTimeMillis();
+
+		System.out.println(
+				String.format("Points: %d, Repeats: %d, Time: %dms", points, repeats,
+						(endTime - startTime)));
+
+		// display image
+		VoronoiBox voronoiBox = Executor.execute(voronoiGenerator, pointBox);
+		Statement<VoronoiBox, BufferedImage> rendererS = new Statement<>(renderer);
+		BufferedImage image = Executor.execute(rendererS, voronoiBox);
 		DialogSingleImage.instance.show(image);
 	}
 }
