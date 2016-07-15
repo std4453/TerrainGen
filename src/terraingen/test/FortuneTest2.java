9bf04b41434ada86eaeb5f740c82ea734763f15a
std4453 <std4453@foxmail.com>
2016-07-15 10:36:17 +0800

    Reform FortuneTest2.java to be more readable and decoupled

--- src/terraingen/test/FortuneTest2.java
+++ src/terraingen/test/FortuneTest2.java
@@ -1,7 +1,6 @@
 package terraingen.test;
 
 import terraingen.backend.commons.Boundaries;
-import terraingen.backend.commons.Point;
 import terraingen.backend.commons.PointBox;
 import terraingen.backend.commons.random.PointsWhiteNoise;
 import terraingen.backend.commons.voronoi.Fortune;
@@ -25,8 +24,8 @@ public class FortuneTest2 implements MouseMotionListener, MouseListener,
 	protected int width = 600;
 	protected int height = 600;
 	protected BufferedImage image;
-	protected Graphics2D g;
 	protected JLabel imageLabel;
+	protected JFrame frame;
 
 	protected Statement<PointBox, VoronoiBox> voronoi;
 	protected PointBox pointBox;
@@ -37,95 +36,152 @@ public class FortuneTest2 implements MouseMotionListener, MouseListener,
 	}
 
 	public FortuneTest2() {
-		final int initial = 80;
+		initData();
+		initUI();
+		showFrame();
+	}
+
+	protected void initData() {
+		final int pointCount = 20;
 		final long seed = 4453;
 
+		// generate initial points
 		Boundaries boundaries = new Boundaries(0, 10, 0, 10);
-		ProcessorNode<Long, PointBox> pointGenerator = new ProcessorNode<>(new
-				PointsWhiteNoise(boundaries, initial));
-		ProcessorNode<PointBox, PointBox> addSamePoint = new ProcessorNode<>(this);
-		new Edge<>(pointGenerator.getOutput(), addSamePoint.getInput());
-		Statement<Long, PointBox> pointGenerator2 = new Statement<>(
-				pointGenerator.getInput(), addSamePoint.getOutput());
-		this.pointBox = Executor.execute(pointGenerator2, seed);
+		ProcessorNode<Long, PointBox> initialPoints = new ProcessorNode<>(new
+				PointsWhiteNoise(boundaries, pointCount));
+		ProcessorNode<PointBox, PointBox> testEdgeCase = new ProcessorNode<>(this);
+		new Edge<>(initialPoints.getOutput(), testEdgeCase.getInput());
+		Statement<Long, PointBox> pointGenerator = new Statement<>(
+				initialPoints.getInput(), testEdgeCase.getOutput());
+		this.pointBox = Executor.execute(pointGenerator, seed);
+
+		// voronoi generator pipeline
 		ProcessorNode<PointBox, VoronoiBox> voronoi = new ProcessorNode<>(new Fortune());
 		this.voronoi = new Statement<>(voronoi);
 
+		// voronoi renderer pipeline
+		this.image = new BufferedImage(this.width, this.height,
+				BufferedImage.TYPE_INT_RGB);
 
-		JFrame frame = new JFrame("Fortune Test 2");
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		// process and render initial PointBox
+		generate();
+	}
+
+	protected void initUI() {
+		// swing components
+		this.frame = new JFrame("Fortune Test 2");
+		this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel();
-		frame.setContentPane(panel);
+		this.frame.setContentPane(panel);
 		panel.setLayout(new BorderLayout());
 		this.imageLabel = new JLabel();
 		panel.add(this.imageLabel, BorderLayout.CENTER);
 		this.imageLabel.setBorder(new CompoundBorder(new EmptyBorder(20, 20, 20, 20), new
 				LineBorder(Color.black)));
-
-		this.image = new BufferedImage(this.width, this.height,
-				BufferedImage.TYPE_INT_RGB);
-		this.g = this.image.createGraphics();
-		generate();
-
+		// ImageIcon in JLabel
 		ImageIcon icon = new ImageIcon(this.image);
 		this.imageLabel.setIcon(icon);
+		updateImage();
 
+		// add input listeners
 		this.imageLabel.addMouseMotionListener(this);
 		this.imageLabel.addMouseListener(this);
-
-		refreshFrame(frame);
-		frame.setVisible(true);
 	}
 
-	protected void refreshFrame(JFrame frame) {
-		frame.pack();
+	protected void showFrame() {
+		this.frame.pack();
+
+		// place frame in the center of the screen
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		Dimension frameSize = frame.getSize();
-		frame.setLocation((int) (screenSize.getWidth() / 2 - frameSize.getWidth() / 2),
+		Dimension frameSize = this.frame.getSize();
+		this.frame.setLocation(
+				(int) (screenSize.getWidth() / 2 - frameSize.getWidth() / 2),
 				(int) (screenSize.getHeight() / 2 - frameSize.getHeight() / 2));
+
+		this.frame.setVisible(true);
 	}
 
-	@Override
-	public void mouseDragged(MouseEvent e) {
+	/**
+	 * process and render the current {@link PointBox}
+	 */
+	protected void generate() {
+		VoronoiBox voronoiBox = Executor.execute(this.voronoi, this.pointBox);
+		VoronoiRenderer.renderToImage(voronoiBox, this.image);
 	}
 
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		terraingen.backend.commons.Point p = this.getPoint(e);
-		if (this.additionalPoint == null) {
-			this.additionalPoint = new Point();
-			this.pointBox.addPoint(this.additionalPoint);
-		}
-		this.additionalPoint.set(p);
-		generate();
-	}
-
-	protected void update() {
+	/**
+	 * update the image in the label
+	 */
+	protected void updateImage() {
 		this.imageLabel.imageUpdate(this.image, ImageObserver.ALLBITS, 0, 0, this.width,
 				this.height);
 	}
 
-	protected void generate() {
-		VoronoiBox voronoiBox = Executor.execute(this.voronoi, this.pointBox);
-		VoronoiRenderer.renderToImage(voronoiBox, this.image);
-		this.update();
-	}
-
+	/**
+	 * Transfer from screen coords to map coords
+	 *
+	 * @param event
+	 * 		{@link MouseEvent} containing screen coords
+	 *
+	 * @return map coords
+	 */
 	protected terraingen.backend.commons.Point getPoint(MouseEvent event) {
 		Insets insets = this.imageLabel.getInsets();
-		int x = event.getX() - insets.top;
-		int y = event.getY() - insets.left;
+		double x = event.getX() - insets.top;
+		double y = event.getY() - insets.left;
 
-		return transfer(x, y);
-	}
-
-	protected terraingen.backend.commons.Point transfer(double x, double y) {
 		Boundaries boundaries = this.pointBox.getBoundaries();
 		double bWidth = boundaries.right - boundaries.left;
 		double bHeight = boundaries.bottom - boundaries.top;
 		return new terraingen.backend.commons.Point(
 				x / this.width * bWidth + boundaries.left,
 				y / this.height * bHeight + boundaries.top);
+	}
+
+	/**
+	 * Test edge case where first two points have the same y coord
+	 *
+	 * @param input
+	 * 		input
+	 *
+	 * @return input with point added
+	 */
+	@Override
+	public PointBox process(PointBox input) {
+		java.util.List<terraingen.backend.commons.Point> points = input.getPoints();
+		double minY = Double.POSITIVE_INFINITY;
+		for (terraingen.backend.commons.Point point : points)
+			if (point.y < minY)
+				minY = point.y;
+
+		input.addPoint(new terraingen.backend.commons.Point(10, minY));
+		return input;
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		terraingen.backend.commons.Point p = this.getPoint(e);
+		if (this.additionalPoint == null) {    // new point to add
+			this.additionalPoint = new terraingen.backend.commons.Point();
+			this.pointBox.addPoint(this.additionalPoint);
+		}
+		this.additionalPoint.set(p);
+		generate();
+		updateImage();
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// as additionalPoint is already added to the PointMap, setting it to null will
+		// only let mouseMoved() to create a new point, as if a point is permanently
+		// inserted into the PointMax
+		this.additionalPoint = null;
+		generate();
+		updateImage();
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent unused) {
 	}
 
 	@Override
@@ -137,28 +193,10 @@ public class FortuneTest2 implements MouseMotionListener, MouseListener,
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent e) {
-		this.additionalPoint = null;
-		generate();
-	}
-
-	@Override
 	public void mouseEntered(MouseEvent e) {
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-	}
-
-	@Override
-	public PointBox process(PointBox input) {
-//		java.util.List<Point> points = input.getPoints();
-//		double minY = Double.POSITIVE_INFINITY;
-//		for (Point point : points)
-//			if (point.y < minY)
-//				minY = point.y;
-//
-//		input.addPoint(new Point(1, minY));
-		return input;
 	}
 }
