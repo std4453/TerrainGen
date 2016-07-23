af910b356d827180b0703797df8ad7f62598c4e7
std4453 <std4453@foxmail.com>
2016-07-23 21:35:00 +0800

    Add original simplex noise by Ken Perlin & test

---
+++ src/terraingen/test/GridSimplexTest.java
@@ -0,0 +1,23 @@
+package terraingen.test;
+
+import terraingen.backend.commons.Boundaries;
+import terraingen.backend.commons.GridRenderer;
+import terraingen.backend.commons.random.GridSimplex;
+import terraingen.backend.nodegraph.Executor;
+import terraingen.frontend.dialog.DialogSingleImage;
+
+import static terraingen.backend.nodegraph.NodeGraphHelper.create;
+import static terraingen.backend.nodegraph.NodeGraphHelper.embrace;
+
+/**
+ *
+ */
+public class GridSimplexTest {
+	public static void main(String[] args) {
+		DialogSingleImage.instance.show(Executor.execute(embrace(
+				create(() -> new Boundaries(10, 10)),
+				create(new GridSimplex(4453, .02)),
+				create(new GridRenderer())
+		)));
+	}
+}
