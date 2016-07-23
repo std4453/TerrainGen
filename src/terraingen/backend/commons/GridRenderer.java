39368acb4f4e9c1fc8260924ac44ac756eac6136
std4453 <std4453@foxmail.com>
2016-07-23 21:28:45 +0800

    Add grid renderer & test

---
+++ src/terraingen/backend/commons/GridRenderer.java
@@ -0,0 +1,121 @@
+package terraingen.backend.commons;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import terraingen.backend.nodegraph.IProcessor;
+import terraingen.utils.MathUtils;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import static terraingen.utils.MathUtils.round;
+
+/**
+ * Renders the {@link Grid} to a {@link BufferedImage}<br /><br />
+ * It actually renders the {@link Grid} with a simple linear interpolation from a given
+ * minimum value and a maximum value to two given colors. For further control of the
+ * image, it should be processed afterwards.
+ */
+public class GridRenderer implements IProcessor<Grid, BufferedImage> {
+	private static final Log log = LogFactory.getLog(GridRenderer.class);
+
+	private static final int DEFAULT_IMAGE_WIDTH = 600;
+	private static final int DEFAULT_IMAGE_HEIGHT = 600;
+
+	public GridRenderer(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public GridRenderer() {
+		this(DEFAULT_IMAGE_WIDTH, DEFAULT_IMAGE_HEIGHT);
+	}
+
+	protected int width;
+	protected int height;
+
+	@Override
+	public BufferedImage process(Grid input) {
+		BufferedImage image = new BufferedImage(this.width, this.height,
+				BufferedImage.TYPE_INT_RGB);
+		renderToImage(input, image);
+		return image;
+	}
+
+	public static void renderToImage(Grid input, BufferedImage image) {
+		// calculates the minimum and maximum value of the Grid
+		double min = Double.POSITIVE_INFINITY;
+		double max = Double.NEGATIVE_INFINITY;
+		int width = input.getWidth();
+		int height = input.getHeight();
+		for (int i = 0; i < width; ++i)
+			for (int j = 0; j < height; ++j) {
+				double value = input.get(i, j);
+				if (value < min)
+					min = value;
+				if (value > max)
+					max = value;
+			}
+
+		renderToImage(min, max, input, image);
+	}
+
+	public static void renderToImage(double min, double max, Grid input,
+									 BufferedImage image) {
+		renderToImage(min, max, null, null, input, image);
+	}
+
+	public static void renderToImage(double min, double max, Color color1, Color color2,
+									 Grid input, BufferedImage image) {
+		if (image == null) {
+			log.warn("Null image given.");
+			return;
+		}
+		if (input == null) {
+			log.warn("Null grid given.");
+			return;
+		}
+
+		if (color1 == null)
+			color1 = Color.BLACK;
+		if (color2 == null)
+			color2 = Color.WHITE;
+
+		Graphics2D g = image.createGraphics();
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+
+		int width = input.getWidth();
+		int height = input.getHeight();
+		Boundaries imageBoundaries = new Boundaries(image.getWidth(), image.getHeight());
+		Boundaries inputBoundaries = new Boundaries(width, height);
+
+		// algorithm 1: render a rectangle for each grid point
+		double rWidth = image.getWidth() / width;
+		double rHeight = image.getHeight() / height;
+		for (int i = 0; i < width; ++i)
+			for (int j = 0; j < height; ++j) {
+				Point renderCenter = MathUtils.transform(inputBoundaries, new Point(i, j)
+						, imageBoundaries);
+				double value = input.get(i, j);
+				Color color = interpolate(min, max, value, color1, color2);
+				g.setColor(color);
+
+				int x = round(renderCenter.x);
+				int y = round(renderCenter.y);
+				// plus-1 hack so that there won't be black stripes
+				int w = round(renderCenter.x + rWidth - x) + 1;
+				int h = round(renderCenter.y + rHeight - y) + 1;
+				g.fillRect(x, y, w, h);
+			}
+	}
+
+	protected static Color interpolate(double min, double max, double value, Color
+			color1, Color color2) {
+		double t = (value - min) / (max - min);
+		double t2 = 1 - t;
+		return new Color((int) (color2.getRed() * t + color1.getRed() * t2),
+				(int) (color2.getGreen() * t + color1.getGreen() * t2),
+				(int) (color2.getBlue() * t + color1.getBlue() * t2));
+	}
+}
---
+++ src/terraingen/test/GridRendererTest.java
@@ -0,0 +1,48 @@
+package terraingen.test;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import terraingen.backend.commons.Grid;
+import terraingen.backend.commons.GridRenderer;
+import terraingen.backend.nodegraph.Executor;
+import terraingen.frontend.dialog.DialogSingleImage;
+import terraingen.utils.ImageUtils;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import static terraingen.backend.nodegraph.NodeGraphHelper.create;
+
+/**
+ *
+ */
+public class GridRendererTest {
+	private static final Log log = LogFactory.getLog(GridRendererTest.class);
+
+	public static void main(String[] args) throws Exception {
+		BufferedImage image = ImageUtils.readInternalImage
+				("/assets/images/test/monalisa.jpg");
+		if (image == null) {
+			log.error("Image not found.");
+			return;
+		}
+		int width = image.getWidth();
+		int height = image.getHeight();
+		int sampling = 5;
+		int sWidth = (width - 1) / sampling;
+		int sHeight = (height - 1) / sampling;
+		int x = 1, y = 1;
+		double data[][] = new double[sWidth][sHeight];
+		for (int i = 0; i < sWidth; ++i, x += sampling) {
+			y = 1;
+			for (int j = 0; j < sHeight; ++j, y += sampling) {
+				Color color = new Color(image.getRGB(x, y));
+				data[i][j] = (color.getRed() + color.getGreen() + color.getBlue()) / 3f;
+			}
+		}
+
+		Grid grid = new Grid(data);
+		GridRenderer renderer = new GridRenderer();
+		DialogSingleImage.instance.show(Executor.execute(create(renderer), grid));
+	}
+}
