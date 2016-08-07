78a3f4cc006c4e3f1e5466cc5e78a0f7c2241675
std4453 <std4453@foxmail.com>
2016-08-07 15:14:40 +0800

    Java-8-ize PointsWhiteNoise.java

--- src/terraingen/backend/commons/noise/PointsWhiteNoise.java
+++ src/terraingen/backend/commons/noise/PointsWhiteNoise.java
@@ -8,7 +8,8 @@ import terraingen.backend.nodegraph.ISupplier;
 
 import java.util.List;
 import java.util.Random;
-import java.util.Vector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Generates a {@link PointBox} with completely random points, i.e., white noise.
@@ -29,12 +30,12 @@ public class PointsWhiteNoise implements IProcessor<Long, PointBox>,
 		double width = this.boundaries.right - this.boundaries.left;
 		double height = this.boundaries.bottom - this.boundaries.top;
 
-		List<Point> points = new Vector<>();
-		for (int i = 0; i < this.pointCount; ++i) {
-			double x = random.nextDouble() * width + this.boundaries.left;
-			double y = random.nextDouble() * height + this.boundaries.top;
-			points.add(new Point(x, y));
-		}
+		List<Point> points = Stream.iterate(0, n -> n + 1).limit(this.pointCount)
+				.parallel().map(n -> {
+					double x = random.nextDouble() * width + this.boundaries.left;
+					double y = random.nextDouble() * height + this.boundaries.top;
+					return new Point(x, y);
+				}).collect(Collectors.toList());
 
 		return new PointBox(this.boundaries, points);
 	}
